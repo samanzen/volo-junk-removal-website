928226b46e3612dbe9c8a73a2fe49579b1e17791
samanzen <kh.sa62@gmail.com>
2025-07-24 23:57:15 -0700

    Create siteData.ts

---
+++ src/data/siteData.ts
@@ -0,0 +1,31 @@
+export const siteData = {
+  businessName: "VOLO JUNK REMOVAL",
+  phone: "(604) 500-2003",
+  phoneHref: "tel:6045002003",
+  email: "contact@volojunk.ca", // Placeholder email
+  address: "727 North Rd, Coquitlam, BC V3J 7L6",
+  googleMapsUrl: "https://maps.google.com/?cid=8120124766353464043&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQ",
+  hours: "Mon–Sun: 7am–10pm",
+  socials: {
+    instagram: "https://www.instagram.com/junkfree.ca/",
+    facebook: "https://www.facebook.com/junkfreevan",
+  },
+  serviceAreas: [
+    "Anmore", "Belcarra", "Burnaby", "Coquitlam", "Delta", "Langley (City)",
+    "Langley (Township)", "Lions Bay", "Maple Ridge", "New Westminster",
+    "North Vancouver (City)", "North Vancouver (District)", "Pitt Meadows",
+    "Port Coquitlam", "Port Moody", "Richmond", "Surrey", "Vancouver",
+    "West Vancouver", "White Rock", "Tsawwassen", "UBC"
+  ],
+  services: [
+    { name: "Furniture Removal", href: "/services/furniture-removal" },
+    { name: "Appliance Removal", href: "/services/appliance-removal" },
+    { name: "Mattress Disposal", href: "/services/mattress-disposal" },
+    { name: "Hot Tub Removal", href: "/services/hot-tub-removal" },
+    { name: "Construction Debris", href: "/services/construction-debris" },
+    { name: "Yard Waste Removal", href: "/services/yard-waste-removal" },
+    { name: "Shed Demolition", href: "/services/shed-demolition" },
+    { name: "Estate Cleanouts", href: "/services/estate-cleanouts" },
+    { name: "Office Cleanouts", href: "/services/office-cleanouts" },
+  ]
+};
