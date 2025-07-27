7c5e2d47b6729080e28282abe956a913182a4113
samanzen <kh.sa62@gmail.com>
2025-07-26 17:48:39 -0700

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: 'YOUR_PROJECT_ID_HERE',
+  projectId: 'zlvpcgia',
   dataset: 'production',
   apiVersion: '2024-02-13', // Use a recent API version
   useCdn: true, // `false` if you want to ensure fresh data
